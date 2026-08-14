2f2ed9cb98b288505b0df2387818f468122cff87
Atharva <chaudhariatharva010@gmail.com>
2026-08-14 10:30:00 +0530

    Add minimal Express server skeleton

---
+++ backend/server.js
@@ -0,0 +1,6 @@
+﻿import express from "express";
+const app = express();
+app.use(express.json());
+app.get("/", (_req, res) => res.json({ ok: true }));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT);
